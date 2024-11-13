6060c6c919345d86253d69c81fc7c9f514536b45
JamesChen <jameschencoding@gmail.com>
2024-11-13 17:39:30 +0800

    feat : Add pages file NotFound

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,8 @@ import Home from "./Home/home";
 import Login from "./Login/login";
 import Map from "./Map/map";
 import Member from "./Member/member";
+import Register from "./Register/register"
+import NotFound from "./NotFound/notFound";
 
 
-export { Home, Login, Map, Member }
+export { Home, Login, Register, Map, Member, NotFound }
