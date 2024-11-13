079b6ffa645d6207fb0ffdf11a46749ad56485fb
JamesChen <jameschencoding@gmail.com>
2024-11-13 17:07:44 +0800

    feat : Tailwind.css setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 
 
+
 function App() {
     return (
-    <>
-      
-    </>
+      <h1 class="text-3xl font-bold underline">
+        Cafe Hunter      
+      </h1>
   )
 }
 
