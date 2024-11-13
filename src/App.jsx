4c65acf320d13a0e5c2542fe422afd8e084582a4
JamesChen <jameschencoding@gmail.com>
2024-11-13 17:13:46 +0800

    feat : Ant Design setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-
+import { useState } from 'react';
+import { Button } from 'antd';
 
 
 function App() {
     return (
+      <>
       <h1 class="text-3xl font-bold underline">
         Cafe Hunter      
       </h1>
+      <Button type="primary">Button</Button>
+      </>
   )
 }
 
