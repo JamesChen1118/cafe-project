923e68695f69f8703ad6f6bc1ac7f4ae546d32f6
JamesChen <jameschencoding@gmail.com>
2024-12-06 13:23:42 +0800

    feat: cluster-icon style setting

--- src/pages/Map/map.jsx
+++ src/pages/Map/map.jsx
@@ -19,16 +19,16 @@ const cafeIcon = L.divIcon({
   html: `<div class="marker-container">
     <svg 
       viewBox="0 0 1024 1024" 
-      width="24" 
-      height="24" 
+      width="32" 
+      height="32" 
       fill="#E74C3C"
     >
       <path d="M275 281c19.9 0 36-16.1 36-36V36c0-19.9-16.1-36-36-36s-36 16.1-36 36v209c0 19.9 16.1 36 36 36zm613 144H768c0-39.8-32.2-72-72-72H200c-39.8 0-72 32.2-72 72v248c0 3.4.2 6.7.7 9.9-.5 7-.7 14-.7 21.1 0 176.7 143.3 320 320 320 160.1 0 292.7-117.5 316.3-271H888c39.8 0 72-32.2 72-72V497c0-39.8-32.2-72-72-72zM696 681h-1.1c.7 7.6 1.1 15.2 1.1 23 0 137-111 248-248 248S200 841 200 704c0-7.8.4-15.4 1.1-23H200V425h496v256zm192-8H776V497h112v176zM613 281c19.9 0 36-16.1 36-36V36c0-19.9-16.1-36-36-36s-36 16.1-36 36v209c0 19.9 16.1 36 36 36zm-170 0c19.9 0 36-16.1 36-36V36c0-19.9-16.1-36-36-36s-36 16.1-36 36v209c0 19.9 16.1 36 36 36z" />
     </svg>
   </div>`,
-  iconSize: [40, 40],
-  iconAnchor: [20, 40],
-  popupAnchor: [0, -40],
+  iconSize: [48, 48],
+  iconAnchor: [24, 48],
+  popupAnchor: [0, -48],
 });
 
 const ClusterLayer = ({ cafes, openModal }) => {
@@ -39,8 +39,8 @@ const ClusterLayer = ({ cafes, openModal }) => {
     const supercluster = new Supercluster({
       radius: 80,
       maxZoom: 16,
-      minZoom: 0,
-      minPoints: 2,
+      minZoom: 8,
+      minPoints: 3,
       nodeSize: 64,
     });
 
@@ -86,17 +86,31 @@ const ClusterLayer = ({ cafes, openModal }) => {
   return clusters.map((cluster) => {
     const [longitude, latitude] = cluster.geometry.coordinates;
     const { cluster: isCluster, point_count: pointCount } = cluster.properties;
+    const zoom = map.getZoom();
 
     if (isCluster) {
       const size =
-        pointCount < 10 ? 64 : pointCount < 25 ? 72 : pointCount < 50 ? 80 : 88;
+        pointCount < 5
+          ? 64 // 5個以下用小圖標
+          : pointCount < 15
+          ? 72 // 5-15個用中圖標
+          : pointCount < 30
+          ? 80 // 15-30個用大圖標
+          : 88; // 30個以上用最大圖標
+
+      const sizeClass =
+        pointCount < 5
+          ? "cluster-icon-small"
+          : pointCount < 15
+          ? "cluster-icon-medium"
+          : "cluster-icon-large";
 
       return (
         <Marker
           key={`cluster-${cluster.id}`}
           position={[latitude, longitude]}
           icon={L.divIcon({
-            className: "cluster-icon",
+            className: `cluster-icon ${sizeClass}`,
             html: `
               <div class="cluster-icon-inner" style="width: ${size}px; height: ${size}px;">
                 <div class="cluster-icon-text">${pointCount}</div>
@@ -120,11 +134,30 @@ const ClusterLayer = ({ cafes, openModal }) => {
       );
     } else {
       const cafe = cafes[cluster.properties.cafeId];
+      const iconSize = zoom >= 16 ? 56 : zoom >= 14 ? 48 : 40;
+
+      const customIcon = L.divIcon({
+        className: "custom-cafe-marker",
+        html: `<div class="marker-container" style="width: ${iconSize}px; height: ${iconSize}px;">
+          <svg 
+            viewBox="0 0 1024 1024" 
+            width="${iconSize * 0.67}"
+            height="${iconSize * 0.67}"
+            fill="#E74C3C"
+          >
+            <path d="M275 281c19.9 0 36-16.1 36-36V36c0-19.9-16.1-36-36-36s-36 16.1-36 36v209c0 19.9 16.1 36 36 36zm613 144H768c0-39.8-32.2-72-72-72H200c-39.8 0-72 32.2-72 72v248c0 3.4.2 6.7.7 9.9-.5 7-.7 14-.7 21.1 0 176.7 143.3 320 320 320 160.1 0 292.7-117.5 316.3-271H888c39.8 0 72-32.2 72-72V497c0-39.8-32.2-72-72-72zM696 681h-1.1c.7 7.6 1.1 15.2 1.1 23 0 137-111 248-248 248S200 841 200 704c0-7.8.4-15.4 1.1-23H200V425h496v256zm192-8H776V497h112v176zM613 281c19.9 0 36-16.1 36-36V36c0-19.9-16.1-36-36-36s-36 16.1-36 36v209c0 19.9 16.1 36 36 36zm-170 0c19.9 0 36-16.1 36-36V36c0-19.9-16.1-36-36-36s-36 16.1-36 36v209c0 19.9 16.1 36 36 36z" />
+          </svg>
+        </div>`,
+        iconSize: [iconSize, iconSize],
+        iconAnchor: [iconSize / 2, iconSize],
+        popupAnchor: [0, -iconSize],
+      });
+
       return (
         <Marker
           key={`cafe-${cluster.properties.cafeId}`}
           position={[latitude, longitude]}
-          icon={cafeIcon}
+          icon={customIcon}
           eventHandlers={{
             click: () => openModal(cafe),
           }}
